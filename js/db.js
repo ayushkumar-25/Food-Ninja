c4e1ebe5ea11d322c207db66ba3d94e479561160
Ayush Kumar <ayush2608@gmail.com>
2021-09-03 16:10:23 +0530

    Sixth Commit🚀
    Firebase offline support and
    "Add new recipe" functionality.

--- js/db.js
+++ js/db.js
@@ -1,3 +1,15 @@
+// enable offline data
+db.enablePersistence()
+    .catch(function (err) {
+        if (err.code == 'failed-precondition') {
+            // probably multible tabs open at once
+            console.log('persistance failed');
+        } else if (err.code == 'unimplemented') {
+            // lack of browser support for the feature
+            console.log('persistance not available');
+        }
+    });
+
 // real-time listener
 db.collection('recipes').onSnapshot((snapshot) => {
     // console.log(snapshot.docChanges());
@@ -11,4 +23,21 @@ db.collection('recipes').onSnapshot((snapshot) => {
             // remove the document data from the web page
         }
     });
+});
+
+// add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const recipe = {
+        title: form.title.value,
+        ingredients: form.ingredients.value
+    };
+
+    db.collection('recipes').add(recipe)
+        .catch(err => console.log(err));
+
+    form.title.value = '';
+    form.ingredients.value = '';
 });
